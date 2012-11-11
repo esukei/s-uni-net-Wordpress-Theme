ee9f79ee2d312552113ffa9300011b9310e99e9b
Satoru Kawahara <satoru@s-uni.net>
2012-11-11 23:10:27 +0900

    fix search result

--- google_site_search.php
+++ google_site_search.php
@@ -4,9 +4,6 @@ Template Name: Google Site Search
 */
 ?>
 <?php get_header();?>
-<nav class="search">
-	<gcse:searchbox-only></gcse:searchbox-only>
-</nav>
 <gcse:searchresults-only></gcse:searchresults-only>
 </div><?php //close Main?>
 <?php get_footer();?>
