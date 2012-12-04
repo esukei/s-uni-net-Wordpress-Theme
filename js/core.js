1e0be37a5f4d62b98c8681cfffd128adbf05c9f9
Satoru Kawahara <satoru@s-uni.net>
2012-12-04 23:39:12 +0900

    alert width

--- js/core.js
+++ js/core.js
@@ -7,33 +7,23 @@
 		var $siteHeader = $('#SiteHeader');
 
 		$win
-			.on({
-				resize: function (event) {
-					alert('r');//console.log('r');
-					if($win.width() < 640) {
-						$siteHeader.css({
-							position: 'absolute',
-							width: 'auto'
-						});
-					}
-					else {
-						$siteHeader.css({
-							position: 'fixed',
-							width: '100%'
-						})
-					}
-				},
-				gestureend: function (event) {
-					alert('wing');//console.log('wing');
-					$win.triggerHandler('resize');
+			.on('resize', function (event) {
+				alert($win.width());//console.log('r');
+				if($win.width() < 640) {
+					$siteHeader.css({
+						position: 'absolute',
+						width: 'auto'
+					});
+				}
+				else {
+					$siteHeader.css({
+						position: 'fixed',
+						width: '100%'
+					})
 				}
 			})
 			.triggerHandler('resize');
 
-		$(doc).on('gestureend', function (event) {
-			alert('doc');//console.log('doc');
-			$win.triggerHandler('resize');
-		});
 	});
 
 }) (this, this.document, jQuery);
