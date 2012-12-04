55135b0e967eb250e8a544e1de92b9f1407fcd6a
Satoru Kawahara <satoru@s-uni.net>
2012-12-04 23:42:03 +0900

    use innerWidth

--- js/core.js
+++ js/core.js
@@ -8,9 +8,7 @@
 
 		$win
 			.on('resize', function (event) {
-				alert($win.width());//console.log('r');
-				alert(win.innerWidth);
-				if($win.width() < 640) {
+				if($win.innerWidth() < 640) {
 					$siteHeader.css({
 						position: 'absolute',
 						width: 'auto'
