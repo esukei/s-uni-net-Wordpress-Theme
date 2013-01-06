ee6ee9d6d1241757f8c12c0b63146f85141eb4f8
Satoru Kawahara <satoru@s-uni.net>
2013-01-06 17:57:29 +0900

    test new width

--- js/core.js
+++ js/core.js
@@ -8,6 +8,17 @@
 		$('meta[name="viewport"]').attr({content: 'width=1024'});
 	}
 
+	var ms_second = 1000,
+		ms_minute = 60000,
+		ms_hour = 3600000,
+		ms_day = 86400000,
+		msToDHMS = function (ms) {
+			var s = ms / ms_second,
+				m = ms / ms_minute,
+				h = ms / ms_hour,
+				d = ms / ms_day
+		};
+
 	$(function(){
 
 		var $siteHeader = $('#SiteHeader');
@@ -28,6 +39,16 @@
 				}
 			})
 			.triggerHandler('resize');
+
+		var $posttime = $('.articleDate')
+			.each(function(i){
+				var $this = $(this);
+				console.log(new Date() - new Date($this.attr('datetime')))
+			});
+
+		setInterval(function(){
+
+		}, 60000);
 
 	});
 
